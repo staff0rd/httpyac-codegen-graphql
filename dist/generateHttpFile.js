29d03de034d2c0f6d9d8703738552f768a5a63a0
Stafford Williams <stafford.williams@gmail.com>
2021-11-24 00:27:00 +1100

    set variables

--- dist/generateHttpFile.js
+++ dist/generateHttpFile.js
@@ -8,10 +8,22 @@ var generateHttpFile = function (operation, config) {
     var headers = "POST ".concat(host, "\nContent-Type: application/json");
     httpFile.push(headers);
     var request = (0, graphql_1.print)(operation);
+    console.log(JSON.stringify(operation, null, 2));
     httpFile.push(request);
-    var variables = config.variables[operation.name.value];
-    if (variables)
-        httpFile.push(JSON.stringify(variables));
+    var schemaVariables = operation.variableDefinitions;
+    var configVariables = config.variables[operation.name.value];
+    if (configVariables || (schemaVariables === null || schemaVariables === void 0 ? void 0 : schemaVariables.length)) {
+        var mandatoryVariables = Object.fromEntries(schemaVariables
+            .filter(function (v) { return v.type.kind === "NonNullType"; })
+            .map(function (v) {
+            var _a;
+            return [
+                v.variable.name.value,
+                (_a = (configVariables || {})[v.variable.name.value]) !== null && _a !== void 0 ? _a : "$".concat(v.variable.name.value),
+            ];
+        }));
+        httpFile.push(JSON.stringify(mandatoryVariables));
+    }
     return httpFile.join("\n\n");
 };
 exports.generateHttpFile = generateHttpFile;
--- src/generateHttpFile.ts
+++ src/generateHttpFile.ts
@@ -12,10 +12,24 @@ export const generateHttpFile = (
   httpFile.push(headers);
 
   const request = print(operation);
+  console.log(JSON.stringify(operation, null, 2));
   httpFile.push(request);
 
-  const variables = config.variables[operation.name.value];
-  if (variables) httpFile.push(JSON.stringify(variables));
+  const schemaVariables = operation.variableDefinitions;
+  const configVariables = config.variables[operation.name.value];
+  if (configVariables || schemaVariables?.length) {
+    const mandatoryVariables = Object.fromEntries(
+      schemaVariables
+        .filter((v) => v.type.kind === "NonNullType")
+        .map((v) => [
+          v.variable.name.value,
+          (configVariables || {})[v.variable.name.value] ??
+            `$${v.variable.name.value}`,
+        ])
+    );
+
+    httpFile.push(JSON.stringify(mandatoryVariables));
+  }
 
   return httpFile.join("\n\n");
 };
